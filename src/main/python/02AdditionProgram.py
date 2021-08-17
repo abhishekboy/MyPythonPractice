f2d8f7d6f157ff97f67edfd8c2cebbe83b4cf5a5
abhishekboy <abhishek.boyanapally@gmail.com>
2021-08-17 12:58:53 -0500

    abc

---
+++ src/main/python/02AdditionProgram.py
@@ -0,0 +1,7 @@
+a='7'
+b='8'
+print (a+b)
+print ('Sum is:'+str(a+b))
+c=a+b
+print(f'Sum is : {c}')
+print('Sum is : {0}{1}'.format(c,a))
---
+++ src/main/python/03Strings.py
@@ -0,0 +1,14 @@
+string1='This is my first String'
+string2=input()
+print(string1[0])
+print(string1[1])
+print(string1[0:3])
+print(string1[5:7])
+print(string1[-1])
+print(string1[-10:])
+print(string1[:7])
+print(string1[6])
+print(len(string1))
+print(string2)
+
+
